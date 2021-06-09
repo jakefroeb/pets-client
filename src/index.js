b45a58ba4d07b92e21d34285584fe0a253669932
jakefroeb <jake.herman.froeb@gmail.com>
2021-06-09 10:05:03 -0500

    added the settup to handle login and register most was copied from previous projects to help get a baseline and for functionality testing on back end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Pets } from './components/Pets';
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Pets/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
